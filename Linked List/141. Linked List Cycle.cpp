3d9320471e823d573ffe9a2465a554a3eb96e502
Archita Das <darchita06@gmail.com>
2025-07-01 15:28:33 +0530

    detecting cycle in a linked list

---
+++ Linked List/141. Linked List Cycle.cpp
@@ -0,0 +1,35 @@
+
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(nullptr) {} };
+ 
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(!head)return false;
+        // map<ListNode*, bool> mp;
+        // ListNode * t=head;
+        // while(t){
+        //     if(mp.count(t))return true;
+        //     mp[t]=true;
+        //     t=t->next;
+        // }
+        // return false;
+
+        ListNode *fast=head;
+        ListNode *slow=head;
+        bool b=false;
+        while(fast && fast->next){
+            fast=fast->next->next;
+            slow=slow->next;
+            if(fast==slow){
+                b=true;
+                break;
+            }
+        }
+        return b;
+        
+
+    }
+};
