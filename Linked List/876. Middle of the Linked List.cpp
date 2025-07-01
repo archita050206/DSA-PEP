e66d396ed51e6fd281855e1502a2c1ee1f503fb0
Archita Das <darchita06@gmail.com>
2025-07-01 14:51:42 +0530

    finding middle element of a linked list

---
+++ Linked List/876. Middle of the Linked List.cpp
@@ -0,0 +1,39 @@
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *middleNode(ListNode *head)
+    {
+        if (!head)
+            return nullptr;
+        // int cnt=0;
+        // ListNode *t=head;
+        // while(t){
+        //     cnt++;
+        //     t=t->next;
+        // }
+        // cnt=cnt/2;
+        // t=head;
+        // for(int i=0;i<cnt;i++){
+        //     t=t->next;
+        // }
+        // return t;
+        ListNode *f = head;
+        ListNode *s = head;
+        while (f && f->next)
+        {
+            s = s->next;
+            f = f->next->next;
+        }
+        return s;
+    }
+};
