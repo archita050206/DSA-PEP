dadb442b809958062a051978ed19a0bfea45d56c
Archita Das <darchita06@gmail.com>
2025-07-01 15:12:09 +0530

    check if ll is circular or not

---
+++ Linked List/Check If Circular Linked Lis.cpp
@@ -0,0 +1,26 @@
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = nullptr;
+    }
+
+};
+
+
+/* Should return true if linked list is circular, else false */
+class Solution {
+  public:
+    bool isCircular(Node *head) {
+        // Your code here
+        if(!head)return false;
+        Node *temp=head->next;
+        while(temp && temp !=head){
+            temp=temp->next;
+        }
+        return temp==head;
+    }
+};
