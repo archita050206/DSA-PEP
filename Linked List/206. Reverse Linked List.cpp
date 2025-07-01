c790d4fbd718858d9bd1d8bef18f1bf371b3b18b
Archita Das <darchita06@gmail.com>
2025-07-01 14:43:21 +0530

    iterative + recursive approach

--- Linked List/206. Reverse Linked List.cpp
+++ Linked List/206. Reverse Linked List.cpp
@@ -11,19 +11,29 @@
 
 class Solution {
 public:
+     ListNode* rev(ListNode * head, ListNode *prev){
+        if(!head)return prev;
+        ListNode* forward=head->next;
+        head->next=prev;
+        return rev(forward,head);
+
+    }
     ListNode* reverseList(ListNode* head) {
-        if(!head)return nullptr;
-        if(head && !head->next)return head;
-        ListNode* prev=nullptr;
-        ListNode* curr=head;
+        // if(!head)return nullptr;
+        // if(head && !head->next)return head;
+        // ListNode* prev=nullptr;
+        // ListNode* curr=head;
         
-        ListNode* forward=nullptr;
-        while(curr){
-            forward=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=forward;
-        }
-        return prev;
+        // ListNode* forward=nullptr;
+        // while(curr){
+        //     forward=curr->next;
+        //     curr->next=prev;
+        //     prev=curr;
+        //     curr=forward;
+        // }
+        // return prev;
+       
+        ListNode * ans=rev(head,nullptr);
+        return ans;
     }
 };
