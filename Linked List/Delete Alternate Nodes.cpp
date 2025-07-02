b27e10f7c57e1d07538c0f078b980ac35fe52e40
Archita Das <darchita06@gmail.com>
2025-07-02 15:18:56 +0530

    delete alternate nodes in a linked list

---
+++ Linked List/Delete Alternate Nodes.cpp
@@ -0,0 +1,39 @@
+
+struct Node
+{
+    int data;
+    struct Node *next;
+
+    Node(int x){
+        int data = x;
+        next = nullptr;
+    }
+};
+
+class Solution {
+  public:
+    void deleteAlt(struct Node *head) {
+        if(!head)return;
+        // Node *first=head->next, *second=head;
+        // while(first){
+        //     second->next=first->next;
+        //     delete first;
+        //     second=second->next;
+        //     if(!second || !second->next)break;
+        //     first=second->next;
+            
+        // }
+        
+        Node *temp=head;
+        while(temp && temp->next){
+            Node *d=temp->next;
+            
+            temp->next=temp->next->next;
+            
+            delete d;
+            temp=temp->next;
+            
+        }
+        
+    }
+};
