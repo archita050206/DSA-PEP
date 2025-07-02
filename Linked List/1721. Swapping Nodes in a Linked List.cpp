9dd494eea4c3f880e436726854c6983e16588a1f
Archita Das <darchita06@gmail.com>
2025-07-02 15:34:51 +0530

    swapping kthe and n-kth node in a ll

---
+++ Linked List/1721. Swapping Nodes in a Linked List.cpp
@@ -0,0 +1,57 @@
+#include<algorithm>
+using namespace std;
+struct ListNode
+{
+    int val;
+
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *swapNodes(ListNode *head, int k)
+    {
+        if (!head)
+            return nullptr;
+        ListNode *first = head, *second = head, *temp = head;
+
+        // int length=0;
+        // while(temp){
+        //     temp=temp->next;
+        //     length++;
+
+        // }
+        // if(k>length){
+        //     return head;
+        // }
+        // for(int i=1;i<k;i++){
+        //     first=first->next;
+        // }
+        // for(int i=0;i<(length-k);i++){
+        //     second=second->next;
+        // }
+        // int t=first->val;
+        // first->val=second->val;
+        // second->val=t;
+        // return head;
+
+        for (int i = 1; i < k; i++)
+        {
+            temp = temp->next;
+        }
+        first = temp;
+        second = head;
+        while (first->next)
+        {
+            first = first->next;
+            second = second->next;
+        }
+        swap(temp->val, second->val);
+        return head;
+    }
+};
