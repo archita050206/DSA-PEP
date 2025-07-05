b148600a02c80593c44794ea02e1c14ba277ccab
Archita Das <darchita06@gmail.com>
2025-07-05 23:15:33 +0530

    commit

--- sliding window/longest subarray with sum k.cpp
+++ sliding window/longest subarray with sum k.cpp
@@ -2,7 +2,6 @@
 #include<algorithm>
 using namespace std;
 int longestSubarrayWithSumK(vector<int> nums, long long k) {
-    // Write your code here
     int i=0,j=0,maxi=-1;
     long long sum=0;
         int n=nums.size();
