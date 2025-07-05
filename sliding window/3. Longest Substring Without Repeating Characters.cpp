9417b264d90be44a8389b2b13a6eb93998a3aba9
Archita Das <darchita06@gmail.com>
2025-07-05 19:56:38 +0530

    longest substring without repeating characters

--- sliding window/3. Longest Substring Without Repeating Characters.cpp
+++ sliding window/3. Longest Substring Without Repeating Characters.cpp
@@ -1,25 +1,23 @@
 #include<climits>
-#include<map>
+#include<unordered_map>
 #include<algorithm>
 using namespace std;
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-        int i=0,j=0;
-        int n=s.length();
-        map<char,bool>mp;
-        int len=0;
+    int lengthOfLongestSubstring(string str) {
+        int i=0,j=0,len=0;
+        int n=str.length();
+        unordered_map<char,bool>mp;
         while(j<n){
-            if(!mp[s[j]]){
-                mp[s[j]]=true;
-                len=max(len,j-i+1);
-                j++;
-            }
-            else if(mp[s[j]]){
-                mp[s[i]]=false;
+            while(mp[str[j]]){
+                mp[str[i]]=false;
                 i++;
             }
+            mp[str[j]]=true;
+            len=max(len, j-i+1);
+            j++;
         }
         return len;
     }
+    
 };
