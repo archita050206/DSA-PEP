485a4b5069608bccc3ed8002b4ab50ac116b5a25
Archita Das <darchita06@gmail.com>
2025-07-07 09:39:38 +0530

    225. Implement Stack using Queues

--- stacks/225. Implement Stack using Queues.cpp
+++ stacks/225. Implement Stack using Queues.cpp
@@ -2,33 +2,45 @@
 using namespace std;
 class MyStack {
 public:
- queue<int>q1,q2;
+ queue<int>q1;
+ // queue<int>q2;
     MyStack() {
        
     }
     
     void push(int x) {
+        // q1.push(x);
         q1.push(x);
+        int s=q1.size();
+        for(int i=1;i<s;i++){
+            int y=q1.front();
+            q1.pop();
+            q1.push(y);
+        }
     }
     
     int pop() {
-        int s=q1.size();
-        for(int i=1;i<s;i++){
-            q2.push(q1.front());
-            q1.pop();
-        }
-        int val=q1.front();
+        // int s=q1.size();
+        // for(int i=1;i<s;i++){
+        //     q2.push(q1.front());
+        //     q1.pop();
+        // }
+        // int val=q1.front();
+        // q1.pop();
+        // while(!q2.empty()){
+        //     q1,push(q2.front());
+        //     q2.pop();
+        // }
+        // return val;
+        int y=q1.front();
         q1.pop();
-        while(!q2.empty()){
-            q1,push(q2.front());
-            q2.pop();
-        }
-        return val;
+        return y;
     }
     
     int top() {
-        int val=q1.back();
-        return val;
+        // int val=q1.back();
+        // return val;
+        return q1.front();
         
     }
     
