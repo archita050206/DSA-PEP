e582c9cc8c5dd5f264cda47a771c3d8bca99d817
Archita Das <darchita06@gmail.com>
2025-07-07 11:28:34 +0530

    2487. Remove Nodes From Linked List

---
+++ stacks/2487. Remove Nodes From Linked List.cpp
@@ -0,0 +1,58 @@
+#include<stack>
+#include<algorithm>
+#include<string>
+
+using namespace std;
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+ 
+class Solution {
+public:
+
+
+    // ListNode* insertAtHead(ListNode* node, ListNode*head){
+      
+    //     if(!head){
+    //         head=node;
+    //         return head;
+    //     }
+    //     node->next=head;
+    //     head=node;
+    //     return head;
+
+    // }
+    ListNode* removeNodes(ListNode* head) {
+        if(!head || !head->next)return head;
+        stack<ListNode*>st;
+        ListNode* t=head;
+        while(t){
+           
+          if(!st.empty() && (st.top()->val < t->val)){
+            st.pop();
+          }
+          else{
+            st.push(t);
+          
+          t=t->next;
+          }
+        }
+        ListNode* h=nullptr;
+        while(!st.empty()){
+            // ListNode* v=st.top();
+            // st.pop();
+            // h=insertAtHead(v,h);
+            t=st.top();
+            st.pop();
+            t->next=h;
+            h=t;
+
+        }
+        return h;
+        
+    }
+};
