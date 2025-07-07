68e60e3c0aa4d11770af5d3df55ddcb4f78e15bd
Archita Das <darchita06@gmail.com>
2025-07-07 10:32:36 +0530

    3174. Clear Digits

---
+++ stacks/3174. Clear Digits.cpp
@@ -0,0 +1,24 @@
+#include<stack>
+#include<algorithm>
+#include<string>
+
+using namespace std;
+class Solution {
+public:
+    string clearDigits(string s) {
+        stack<char>st;
+        for(char c: s){
+            if(isdigit(c) && isalpha(st.top())){
+                st.pop();
+            }
+            else st.push(c);
+        }
+        string ans="";
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
