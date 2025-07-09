b4b9621ffa8dae4e3a84e4d2b6aa7dc9fe9ff97b
Archita Das <darchita06@gmail.com>
2025-07-09 10:28:52 +0530

    Brian Kermighan's Algorithm

--- bit manipulation/191. Number of 1 Bits.cpp
+++ bit manipulation/191. Number of 1 Bits.cpp
@@ -1,11 +1,21 @@
 class Solution {
 public:
     int hammingWeight(int n) {
+
         int cnt=0;
         while(n!=0){
             if((n&1)==1)cnt++;
             n=n>>1;
         }
         return cnt;
+
+        // Brian Kermighan's Algorithm
+        // while(n>0){
+        //     n=n&(n-1);
+        //     cnt++;
+        // }
+        // return cnt;
+
+       
     }
 };
