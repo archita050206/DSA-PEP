7bd230ea946db5b153194ed087f8821d07446da1
Archita Das <darchita06@gmail.com>
2025-07-10 10:50:55 +0530

    1846. Maximum Element After Decreasing and Rearranging

---
+++ greedy/1846. Maximum Element After Decreasing and Rearranging.cpp
@@ -0,0 +1,24 @@
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+        int n=arr.size();
+        if(arr[0]!=1)arr[0]=1;
+
+        for(int i=1;i<n;i++){
+            if(abs(arr[i]-arr[i-1])>1){
+                arr[i]=arr[i-1]+1;
+            }
+        }
+
+        int maxi=0;
+        for(int i=0;i<n;i++){
+            maxi=max(maxi,arr[i]);
+        }
+        return maxi;
+    }
+};
