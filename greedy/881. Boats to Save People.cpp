fcbbce8557e4e514e2e25eaa0d42e2d0c781c3aa
Archita Das <darchita06@gmail.com>
2025-07-10 09:39:36 +0530

    881. Boats to Save People

---
+++ greedy/881. Boats to Save People.cpp
@@ -0,0 +1,22 @@
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+        int i=0,cnt=0;
+        int j=people.size()-1;
+        while(i<=j){
+        if((people[i]+people[j])<=limit){
+            
+            i++;
+            j--;
+        }
+        else{
+            j--;
+        }
+        cnt++;
+        }
+        return cnt;
+    }
+};
