651f567a28482b180fed3f76a8180c5e657cfd7a
Archita Das <darchita06@gmail.com>
2025-07-10 23:18:53 +0530

    435. Non-overlapping Intervals

---
+++ greedy/435. Non-overlapping Intervals.cpp
@@ -0,0 +1,22 @@
+#include<vector>
+#include<algorithm>
+using namespace std;
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        int cnt=0;
+        sort(intervals.begin(),intervals.end(),[](const vector<int>&a,const vector<int>&b){
+            return a[1]<b[1];
+        });
+        int end=intervals[0][1];
+        for(int i=1;i<intervals.size();i++){
+            if((intervals[i][0]<end) ){
+                cnt++;
+
+            }
+            else
+            end=intervals[i][1];
+        }
+        return cnt;
+    }
+};
