0e61f808f9692c8f62f3c72e83a9938892d5cbd9
Archita Das <darchita06@gmail.com>
2025-07-10 10:59:50 +0530

    returning the last ele instead of traversing the whole array

--- greedy/1846. Maximum Element After Decreasing and Rearranging.cpp
+++ greedy/1846. Maximum Element After Decreasing and Rearranging.cpp
@@ -1,24 +1,29 @@
-#include<vector>
-#include<algorithm>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
-        sort(arr.begin(),arr.end());
-        int n=arr.size();
-        if(arr[0]!=1)arr[0]=1;
+    int maximumElementAfterDecrementingAndRearranging(vector<int> &arr)
+    {
+        sort(arr.begin(), arr.end());
+        int n = arr.size();
+        if (arr[0] != 1)
+            arr[0] = 1;
 
-        for(int i=1;i<n;i++){
-            if(abs(arr[i]-arr[i-1])>1){
-                arr[i]=arr[i-1]+1;
+        for (int i = 1; i < n; i++)
+        {
+            if (abs(arr[i] - arr[i - 1]) > 1)
+            {
+                arr[i] = arr[i - 1] + 1;
             }
         }
 
-        int maxi=0;
-        for(int i=0;i<n;i++){
-            maxi=max(maxi,arr[i]);
-        }
-        return maxi;
+        // int maxi=0;
+        // for(int i=0;i<n;i++){
+        //     maxi=max(maxi,arr[i]);
+        // }
+        return arr[n - 1];
     }
 };
