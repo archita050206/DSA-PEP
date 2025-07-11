abc5e76ec2453e7c03b614b5ad9cf97f15b71a37
Archita Das <darchita06@gmail.com>
2025-07-11 10:09:31 +0530

    56. Merge Intervals

---
+++ greedy/56. Merge Intervals.cpp
@@ -0,0 +1,22 @@
+#include<vector>
+#include<algorithm>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>>ans;
+        
+        for(int i=0;i<intervals.size();i++){
+           if(ans.empty() || ans.back()[1]<intervals[i][0]){
+            ans.push_back(intervals[i]);
+           }
+       
+            else if(ans.back()[1]>=intervals[i][0]){
+                ans.back()[1]=max(ans.back()[1],intervals[i][1]);
+            }
+           
+        }
+        return ans;
+    }
+};
