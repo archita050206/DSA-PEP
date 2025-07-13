cfb91ec56654f0d6e3d03c16d1b06a269fd56016
Archita Das <darchita06@gmail.com>
2025-07-13 20:17:12 +0530

    216. Combination Sum III

---
+++ recursion/216. Combination Sum III.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void solver(int idx, vector<int>&temp,vector<vector<int>>&ans, int k, int n){
+        if(k==0 && n==0){
+            ans.push_back(temp);
+            return;
+
+        }
+        if(k==0 || n<0)return;
+        for(int i=idx;i<=9;i++){
+           temp.push_back(i);
+           solver(i+1,temp,ans,k-1,n-i);
+            temp.pop_back();
+
+            
+        }
+    }
+
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>>ans;
+        vector<int>temp;
+        vector<int>nums;
+        
+        solver(1,temp,ans,k,n);
+        return ans;
+    }
+};
