a9adf2f7761a5cd9fab7b35b5ec680171edc4511
Archita Das <darchita06@gmail.com>
2025-07-14 11:38:29 +0530

    added an iterative approach for preorder traversal of tree

--- binary tree/144. Binary Tree Preorder Traversal.cpp
+++ binary tree/144. Binary Tree Preorder Traversal.cpp
@@ -1,4 +1,6 @@
 #include<vector>
+#include<stack>
+
 using namespace std;
  struct TreeNode {
      int val;
@@ -11,6 +13,9 @@ using namespace std;
  
 class Solution {
 public:
+
+    //Recursive way
+    
     void preorder(TreeNode *root, vector<int>&v){
         if(!root)return;
         v.push_back(root->val);
@@ -22,4 +27,21 @@ public:
         preorder(root,v);
         return v;
     }
+
+    //Iterative way
+
+    // vector<int> preorderTraversal(TreeNode* root) {
+    //     vector<int>v;
+    //     stack<TreeNode *>st;
+    //     if(!root)return v;
+    //     st.push(root);
+    //     while(!st.empty()){
+    //         TreeNode *node=st.top();
+    //         st.pop();
+    //         if(node->right)st.push(node->right);
+    //         if(node->left)st.push(node->left);
+    //         v.push_back(node->val);
+    //     }
+    //     return v;
+    // }
 };
