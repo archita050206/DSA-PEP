70a8d758e62d84556cd3da9da96b7280611e0efd
Archita Das <darchita06@gmail.com>
2025-07-23 09:43:34 +0530

    Undirected Graph Cycle

---
+++ graphs/Undirected Graph Cycle.cpp
@@ -0,0 +1,46 @@
+#include<vector>
+#include<queue>
+using namespace std;
+class Solution {
+  public:
+    bool bfs(int n, vector<bool>&visited,vector<vector<int>>&adj){
+        queue<pair<int,int>>q;
+        
+        q.push({n,-1});
+        visited[n]=true;
+        while(!q.empty()){
+            int node=q.front().first;
+            int parent=q.front().second;
+            q.pop();
+            for(auto &i: adj[node]){
+                if(visited[i] && i!=parent)return true;
+           
+                 else   if(!visited[i]){
+                        q.push({i,node});
+                        visited[i]=true;
+                    }
+                
+            }
+        }
+        return false;
+        
+    }
+    bool isCycle(int V, vector<vector<int>>& edges) {
+        // Code here
+        vector<vector<int>>adj(V);
+        vector<bool>visited(V,false);
+        for(int i=0;i<edges.size();i++){
+            int u=edges[i][0];
+            int v=edges[i][1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        for(int i=0;i<V;i++){
+            if(!visited[i]&& bfs(i,visited,adj)){
+                return true;
+            }
+        }
+        return false;
+        
+    }
+};
