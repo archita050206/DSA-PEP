9d5ffb40324b0d47176a741859ce7a5ad88904fa
Archita Das <darchita06@gmail.com>
2025-07-24 10:27:37 +0530

    Topological Sort

---
+++ graphs/Topological Sort.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h> 
+
+void dfs(int node,vector<bool>&visited, stack<int>&st, vector<vector<int>>&adj ){
+    visited[node]=true;
+    for(int child: adj[node]){
+        if(!visited[child]){
+            dfs(child,visited,st,adj);
+        }
+    }
+    st.push(node);
+}
+vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  {
+    
+    stack<int>st;
+    vector<bool>visited(v);
+    vector<vector<int>>adj(v);
+    for(int i=0;i<e;i++){
+        int u=edges[i][0];
+        int v=edges[i][1];
+        adj[u].push_back(v);
+    }
+    for(int i=0;i<v;i++){
+        if(!visited[i]){
+            dfs(i,visited,st,adj);
+        }
+    }
+    vector<int>ans;
+    while(!st.empty()){
+        ans.push_back(st.top());
+        st.pop();
+    }
+    return ans;
+}
