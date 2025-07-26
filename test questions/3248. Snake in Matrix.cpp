fea7b57bcbb6da22afe40926c7229af501dbe90c
Archita Das <darchita06@gmail.com>
2025-07-26 09:57:48 +0530

    3248. Snake in Matrix

---
+++ test questions/3248. Snake in Matrix.cpp
@@ -0,0 +1,17 @@
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int finalPositionOfSnake(int n, vector<string>& commands) {
+            
+            int i=0,j=0;
+           
+            for(int k=0;k<commands.size();k++){
+                if(commands[k]=="UP" && i-1>=0)i=i-1;
+                else if(commands[k]=="DOWN" && i+1<n)i=i+1;
+                else if(commands[k]=="LEFT" && j-1>=0)j=j-1;
+                else if(commands[k]=="RIGHT" && j+1<n)j=j+1;
+            }
+            return (i*n)+j;
+    }
+};
