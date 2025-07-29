1fc1309ae6cfb42fecf56b3ba3057e7b37ab5476
Archita Das <darchita06@gmail.com>
2025-07-29 14:45:33 +0530

    solved using memoisation

--- recursion/Perfect Sum Problem.cpp
+++ recursion/Perfect Sum Problem.cpp
@@ -11,10 +11,26 @@ class Solution {
         }
         return solver(i+1, arr, target-arr[i], n)+solver( i+1,arr, target,n);
     }
-  
+    int solver(vector<int>& arr, int target, vector<vector<int>>&dp, int n){
+      
+      if(n==0)
+     {
+         if(target==0)return 1;
+         return 0;
+     }
+      
+      if(dp[n][target]!=-1)return dp[n][target];
+      if(arr[n-1]<=target){
+          dp[n][target]=solver(arr, target-arr[n-1],dp,n-1)+solver(arr,target,dp,n-1);
+      }
+      else dp[n][target]=solver(arr,target,dp,n-1);
+      return dp[n][target];
+  }
     int perfectSum(vector<int>& arr, int target) {
       
         int n=arr.size();
-        return solver(0,arr,target,n);
+        //return solver(0,arr,target,n);
+        vector<vector<int>>dp(n+1, vector<int>(target+1,-1));
+        return solver(arr, target,dp,n);
     }
 };
