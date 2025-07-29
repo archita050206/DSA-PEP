e0c920f776c0250bf226c03c41ed0e20d76c9ffc
Archita Das <darchita06@gmail.com>
2025-07-29 11:07:29 +0530

    solved using memoisation

--- recursion/Subset Sum Problem.cpp
+++ recursion/Subset Sum Problem.cpp
@@ -21,7 +21,26 @@ public:
     bool isSubsetSum(vector<int> &arr, int target)
     {
         // code here
-        int n = arr.size();
-        return solver(0, arr, target, n);
+        // int n = arr.size();
+        // return solver(0, arr, target, n);
+        int n=arr.size();
+        //return solver(0,arr,target,n);
+        vector<vector<bool>>dp(n+1,vector<bool>(target+1));
+        
+        for(int i=0;i<target;i++){
+            dp[0][i]=false;
+        }
+        for(int i=0;i<n;i++){
+            dp[i][0]=true;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=target;j++){
+                if(arr[i-1]<=j){
+                    dp[i][j]=dp[i-1][j-arr[i-1]]||dp[i-1][j];
+                }
+                else dp[i][j]=dp[i-1][j];
+            }
+        }
+        return dp[n][target];
     }
 };
