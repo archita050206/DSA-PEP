d52f3c56eb83e7146a3704f35c48ed1a72d11a18
Archita Das <darchita06@gmail.com>
2025-10-23 16:38:06 +0530

    Least Prime Factor

---
+++ CP/Least Prime Factor.cpp
@@ -0,0 +1,22 @@
+#include<vector>
+#include<string>
+using namespace std;
+class Solution {
+  public:
+    vector<int> leastPrimeFactor(int n) {
+        // code here
+        vector<int>ans(n+1,0);
+        ans[1]=1;
+        for(int i=2;i<=n;i++){
+            if(ans[i]==0){
+                ans[i]=i;
+                for(int j=i*i;j<=n;j+=i){
+                    if(ans[j]==0){
+                        ans[j]=i;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
