e357b49587fa782d8e4bdfed55a3e32c26b46905
Archita Das <144708429+archita050206@users.noreply.github.com>
2025-11-16 01:13:24 +0530

    322. Coin Change

---
+++ dynamic programming/322. Coin Change.cpp
@@ -0,0 +1,25 @@
+#include<vector>
+#include<climits>
+using namespace std;
+class Solution {
+public:
+    int solver(int amount, vector<int>&coins, vector<int>&dp){
+        if(amount==0)return 0;
+        if(amount<0)return INT_MAX;
+        if(dp[amount]!=-1)return dp[amount];
+        int mini=INT_MAX;
+        for(int c: coins){
+            int res=solver(amount-c, coins, dp);
+            if(res<INT_MAX){
+                mini=min(mini,res+1);
+            }
+        }
+        return dp[amount]=mini;
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int>dp(amount+1,-1);
+        int ans=solver(amount, coins, dp);
+        return ans==INT_MAX?-1:ans;
+    }
+
+};
