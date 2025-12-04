1090f1b76ffe0ccc5d4b4a4023cf9cc9423b25bd
Archita Das <144708429+archita050206@users.noreply.github.com>
2025-12-04 15:35:29 +0530

    nCr

--- dynamic programming/nCr.cpp
+++ dynamic programming/nCr.cpp
@@ -11,7 +11,15 @@ class Solution {
     }
     int nCr(int n, int r) {
         // code here
-        vector<vector<int>>dp(n+1,vector<int>(r+1,-1));
-        return fun(n,r,dp);
+         vector<vector<int>>dp(n+1,vector<int>(r+1,0));
+        // return fun(n,r,dp);
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=r;j++){
+                if(j==0 || j==i)dp[i][j]=1;
+                else if(j>i)dp[i][j]=0;
+                else dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+            }
+        }
+        return dp[n][r];
     }
 };
