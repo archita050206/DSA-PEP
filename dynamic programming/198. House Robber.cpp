b0c5802b6d23fc6e47b6484d3ffc0f6eae864bcb
Archita Das <144708429+archita050206@users.noreply.github.com>
2025-12-12 01:54:59 +0530

    simplified condition

--- dynamic programming/198. House Robber.cpp
+++ dynamic programming/198. House Robber.cpp
@@ -33,6 +33,7 @@ public:
         dp[1] = max(nums[0], nums[1]);
         for (int i = 2; i < n; i++)
         {
+            //  dp[i]=max(nums[i]+dp[i-2], dp[i-1]);
             int steal = nums[i] + dp[i - 2];
             int skip = dp[i - 1];
             dp[i] = max(steal, skip);
