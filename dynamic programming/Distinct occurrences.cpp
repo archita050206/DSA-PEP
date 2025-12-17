fa1657a2bedb2d6e241bc0d312a9fa4c08e2b71f
Archita Das <144708429+archita050206@users.noreply.github.com>
2025-12-17 20:52:55 +0530

    Distinct occurrences

---
+++ dynamic programming/Distinct occurrences.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+#include<climits>
+using namespace std;
+class Solution {
+  public:
+    int subseqCount(string &txt, string &pat) {
+        // code here
+        int n=txt.length();
+        int m=pat.length();
+        vector<vector<int>>dp(n+1, vector<int>(m+1,0));
+        for(int i=0;i<=n;i++){
+            dp[i][0]=1;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(txt[i-1]==pat[j-1]){
+                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                }
+                else dp[i][j]=dp[i-1][j];
+            }
+        }
+        return dp[n][m];
+    }
+};
