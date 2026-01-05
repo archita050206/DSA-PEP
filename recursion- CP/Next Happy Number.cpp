f3a52f0a7239e651c70dcfb9058b547fce4ef393
Archita Das <144708429+archita050206@users.noreply.github.com>
2026-01-06 03:03:06 +0530

    solved using a simpler method

--- recursion- CP/Next Happy Number.cpp
+++ recursion- CP/Next Happy Number.cpp
@@ -2,32 +2,55 @@
 using namespace std;
 class Solution {
   public:
-    int sumOfSquares(int n){
-        int s=0;
+    // int sumOfSquares(int n){
+    //     int s=0;
+    //     while(n!=0){
+    //         int d=n%10;
+    //         s+=d*d;
+    //         n=n/10;
+    //     }
+    //     return s;
+    // }
+    // bool isHappy(int n, unordered_set<int>&visited){
+    //     if(n==1)return true;
+    //     if(visited.count(n))return false;//loop exists
+    //     visited.insert(n);
+    //     int next=sumOfSquares(n);
+    //     return isHappy(next,visited);
+    // }
+    // int nextHappy(int N) {
+    //     // code here
+       
+    //     int x=N+1;
+    //     while(true){
+    //          unordered_set<int>visited;
+    //         if(isHappy(x,visited))return x;
+    //         x++;
+    //     }
+    //     return -1;
+        
+    // }
+
+    
+  public:
+    bool solver(int n){
+        if(n==1 || n==7)return true;
+        if(n<10)return false;
+        int sum=0;
         while(n!=0){
             int d=n%10;
-            s+=d*d;
+            sum+=(d*d);
             n=n/10;
         }
-        return s;
-    }
-    bool isHappy(int n, unordered_set<int>&visited){
-        if(n==1)return true;
-        if(visited.count(n))return false;//loop exists
-        visited.insert(n);
-        int next=sumOfSquares(n);
-        return isHappy(next,visited);
+        return solver(sum);
     }
     int nextHappy(int N) {
         // code here
-       
-        int x=N+1;
         while(true){
-             unordered_set<int>visited;
-            if(isHappy(x,visited))return x;
-            x++;
+            N++;
+            if(solver(N))return N;
         }
         return -1;
-        
-    }
+    
+};
 };
