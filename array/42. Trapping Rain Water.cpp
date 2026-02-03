fbd4611f776681a44a4c253e32367e4c25b482ce
Archita Das <144708429+archita050206@users.noreply.github.com>
2026-02-04 02:09:29 +0530

    42. Trapping Rain Water

---
+++ array/42. Trapping Rain Water.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include<string>
+#include<unordered_map>
+#include<algorithm>
+using namespace std;
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n=height.size(),ans=0;
+        int l=0,r=n-1,left_max=0,right_max=0;
+        while(l<r){
+            if(height[l]<=height[r]){
+            if(height[l]>=left_max){
+                left_max=height[l];
+            }
+            else{
+                ans+=(left_max-height[l]);
+            }
+            l++;
+            }
+            else{
+                if(height[r]>=right_max){
+                    right_max=height[r];
+                }
+                else{
+                    ans+=(right_max-height[r]);
+                }
+                r--;
+            }
+        }
+        return ans;
+    }
+};
