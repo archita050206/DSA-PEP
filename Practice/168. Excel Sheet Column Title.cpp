6c2e52baf97c1358e5f0c588dede4f114fa3e585
Archita Das <144708429+archita050206@users.noreply.github.com>
2026-02-06 16:54:09 +0530

    added a simpler approach

--- Practice/168. Excel Sheet Column Title.cpp
+++ Practice/168. Excel Sheet Column Title.cpp
@@ -4,20 +4,24 @@ using namespace std;
 class Solution {
 public:
     string convertToTitle(int columnNumber) {
-        string ans;
-       
-        while(columnNumber>=1){
+        string ans="";
+        
+        while(columnNumber>0){
+            columnNumber-=1;
             int d=columnNumber%26;
-            if(d==0)
-            {ans.push_back('Z');
-            columnNumber=columnNumber/26-1;
+            char c=d+'A';
+            ans+=c;
+            columnNumber/=26;
+            // if(d==0)
+            // {ans.push_back('Z');
+            // columnNumber=columnNumber/26-1;
             
-            }
-            else {
-                char c= d+'A'-1;
-                ans.push_back(c);
-                columnNumber=columnNumber/26;
-            }
+            // }
+            // else {
+            //     char c= d+'A'-1;
+            //     ans.push_back(c);
+            //     columnNumber=columnNumber/26;
+            // }
             
 
         }
