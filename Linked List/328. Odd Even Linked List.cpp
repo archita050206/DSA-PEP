8c07881aa6e85866d2de2c2f45cd9bfd4b4e891a
Archita Das <144708429+archita050206@users.noreply.github.com>
2026-02-13 00:35:58 +0530

    328. Odd Even Linked List

---
+++ Linked List/328. Odd Even Linked List.cpp
@@ -0,0 +1,26 @@
+
+  
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(!head || !head->next)return head;
+        ListNode *oH=head,*o=head,*e=head->next,*eH=head->next;
+        while(e && e->next){
+            o->next=e->next;
+            o=o->next;
+            e->next=o->next;
+            e=e->next;
+        }
+        o->next=eH;
+        return oH;
+
+    }
+};
