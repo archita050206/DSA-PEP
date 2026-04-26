8c25220ee8d2285aae97751dc9a2c17f2c5df89a
Archita Das <144708429+archita050206@users.noreply.github.com>
2026-04-26 16:31:27 +0530

    147. Insertion Sort List

---
+++ sorting CP/147. Insertion Sort List.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        if(!head)return nullptr;
+        ListNode *curr=head,*sorted=nullptr;
+        while(curr){
+            ListNode *next=curr->next;
+            if(!sorted || curr->val<sorted->val){
+                curr->next=sorted;
+                sorted=curr;
+            }
+            else{
+                ListNode *temp=sorted;
+                while(temp->next && temp->next->val<curr->val){
+                    temp=temp->next;
+                }
+                curr->next=temp->next;
+                temp->next=curr;
+            }
+            curr=next;
+        }
+        return sorted;
+    }
+};
